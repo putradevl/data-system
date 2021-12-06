7fda335a759aceeac6f31eca9646c67a8c81f69a
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-12-06 17:04:35 +0700

    Create disable.js

---
+++ webasset/www/disable.js
@@ -0,0 +1,163 @@
+var domainuc='clk.sh,powerdatatech.com,midiavip.com,subsvip.com,hulkshare.com,sharebeast.com,fir3.net,ads.enrt.eu,seulink.net,stfly.me,exe.io,adfoc.us,orablyro.com,hearthis.at,gestyy.com,tenteaea.com,filefox.cc,stfly.me,ivononic.com,orablyro.com,chl.li,acconpit.com,criarysm.com,stfly.me,direct.bsgspl.xyz,cutly.com.br,anunciad.com.br,raboninco.com,exe.io,download1505.mediafire.com,evassmat.com,spreaker.com,api.spreaker.com,meriabub.net,adpllus.com,hearthis.at,gestyy.com,fir3.net,cutwin.com,2shrt.com,upload-earn.com,cutedrive.com,babblecase.com,hearthis.at,anonym.to,oload.host,acortar.live,infopade.com,www24.zippyshare.com,dbree.co,midiashared.com,genesistab.com,gounlimited.to,raboninco.com,direct.bsgspl.xyz,dopefile.me,lirikterjamahanindo.com,fir3.net,www.udemy.com,udemy.com,isotonikstudios.com,lfs.net,ok.ru,infopade.com.xerver.co,mixdrop.to,feurl.com,123filmes.online,openvid.xyz,raboninco.com,yoalizer.com,new.vk.com,net-load.com,dood.to,cleanuri.com,smwebs.xyz,tenteaea.com,cloud.mail.ru,fanburst.com,pintient.com,cutt.ly,tr.link,mega.nz,drivefire.co,yepuploadin.com,tirnaice2th.parsaspace.com,yepuploadin.com,srv15.donya.co,firebasestorage.googleapis.com,uwatchfree.ac,dopefile.me,t.me,hearthis.at,dapalan.com,cismp3.org,turboagram.com,direct.bsgspl.xyz,suaurl.com,gounlimited.to,upstream.to,allreports.site,acortar.live,hinafinea.com,dopefile.me,abre.ai,pladollmo.com,fir3.net,scadonsak.com,sluppend.com,zipansion.com,vivads.net,cpmlink.net,url.gratis,fir3.net,cismp3.org,cloudup.com,go99tech.com,hearthis.at,fir3.net,short88.com,linkvertise.com,shrtfly.net,2shrt.ml,safelinku.com,semawur.com,x7.to,taraa.xyz,vidoza.net,mgnet.me,mixdrop.co,eunsetee.com,tekclicks.tech,k2s.cc,stiffvip.com.download.megafono.host,encurta.eu,usersdrive.com,enrt.eu,eunsetee.com,download2268.mediafire.com,download2262.mediafire.com,link-to.net,bc.vc,encurtatop.tech,flic.kr,ift.tt,encurtatop.tech,midvip.xyz,j.gs,r4dm.com,eunsetee.com,verystream.com,fanburst.com,musicaquente10.blogspot.com,verystream.com,fc.lc,thacorag.com,stfly.io,miniurl.com.br,enrt.eu,d2.bsgspl.xyz,bsgspl.xyz,direct.bsgspl.xyz,nofile.io,xterca.net,tonancos.com,xterca.net,ez4linkss.com,thepiratebay.ee,torcache.net,clicksfly.com,seriesonlinehd.org,gsur.in,file4.net,miniurl.com.br,www.miniurl.com.br,evassmat.com,uplod.net,miniurl.com.br,file-up.org,icedrive.net,bayfiles.com,openload.cc,anonfile.com,filmesonlinegratis.net,urlcero.co,ncrypt.in,ecleneue.com,cesinthi.com,lopoteam.com,vaugette.com,enrt.eu,sowernal.com,dl.torrentcrazy.com,redecanais.tv,sluppend.com,sluppend.com,quamiller.com,lopoteam.com,turboagram.com,sluppend.com,2shrt.com,icecreamapps.com,mgid.com,ruvid.nl,dwindly.io,animalog.biz,theblacklistbrasil.tk,twiriock.com,ethobleo.com,onisedeo.com,dailymotion.com,swifttopia.com,adfoc.us,link5s.co,appsgeyser.com,app.appsgeyser.com,safelinkblogger.com,animalog.biz,s000.tinyupload.com,covelign.com,new.vk.com,vk.com,el3id.site,thenightanimes.net,covelign.com,uclaut.net,lnk.do,kidsors.xyz,won.pe,sendit.cloud,dl.free.fr,download.jheberg.net,mirrorace.com,filerio.in,hitfile.net,dailyuploads.net,qfiles.io,multiup.org,megaup.net,idsly.bid,clipwatching.com,soratemplates.com,box.com,m.box.com,za.gl,ez4link.net,solidfile.net,lyon.kim,4to.xyz,mgnet.me,1drv.ms,adflyk.com,dl-file.com,fanburst.com,dapalan.com,10convert.com,enrt.eu,baixar.down.mus.br,cogismith.com,uii.io,cinebo.net,dapalan.com,filesify.com,stfly.press,wicr.me,animeszx.com.br,url.animeszx.com.br,1fichier.com,stratoplot.com,q.gs,masterads.info,shon.xyz,combostruct.com,rapidteria.com,idsly.bid,safelinku.com,idsly.bid,yoalizer.com,fanburst.com,od.lk,swiftation.com,adyou.me,mega.gratis,fanburst.com,app.box.com,linkshrink,fanburst.com,brasfoot.com,fanburst.com,adflyk.com,mega.nz,mega.com,mega.net,minup.net,yamechanic.com,velociterium.com,d1.bsg.cloud,yunfile.com,page2.yunfile.com,page4.dfpan.com,mixstep.co,dbr.ee,ghostbin.com,clearload.bid,rapidteria.com,zipansion.com,aquelesom.com,itunes.apple.com,apkpure.com,kaitect.com,brisktopia.com,musio.net.br,yoineer.com,ir-dl.com,dl.ir-dl.com,corneey.com,nimbleinity.com,rapidtory.com,agileurbia.com,activeterium.com,zipvale.com,festyy.com,velocicosm.com,musio.net.br,swiftation.com,vializer.com,jzrputtbut.net,yobuilder.com,quainator.com,rgho.st,od.lk,adfoc.us,adult.xyz,zo.ee,openlink.biz,openload.co,drive.google.com,feedify.com.br,j.gs,gestyy.com,sistemasertanejo.com,pomfe.co,a.pomfe.co,moddisc.com,picocurl.com,shink.in,shink,ir.thepirategratis.com,zoink.it,ay.gy,viahold.com,1fichier.com,dataurbia.com,casualient.com,adult.xyz,ceesty.com,encurta.net,download.psdirect.us,midiashared.com,s000.tinyupload.com,clkmein.com,destyy.com,shink.in,limetorrents.cc,weshare.me,superdowns.org,apkmirror.com,zofile.com,mirrorcreator.com,solidfiles.com,sht.io,mobdisc.comupfile.mobi,play.google.com,ahref.co,bluenik.com,microify.com,babblecase.com,encurtar.linkfanburst.com,play.mob.org.pt,play.mob.orgbigfile.to,fileboxer.co.za,tinyurl.com,bluenik.com,fas.li,engine.adbooth.com,ift.tt,bc.vc,multcloud.com,viid.me,1fichier.com,github.com,linkshrink.net,katfile.com,iouk.biz,sharepoint.com,hitsebeats-my.sharepoint.com,novomp3.net.br,teucloud.com,href.li,userscloud.com,binbox.io,jheberg.net,midiashared.com,dfiles.eu,linkurl.xyz,bigfile.to,urlcash.net,urlf.ly,notlong.com,main.exoclick.com,magnet:?xt,upload.cash,uskip.me,brfiles.com,openload.co,spaste.com,d-h.st,uploaded,24uploading,24uploading.com,bitshare,ul.to, pastebin,24uploading,freakshare,anyhub,oron,brarquivos,oboom,123savemp3,salefiles,storbit,userscloud,cloudbrasil,meocloud,soundowl,meocloud.pt,secureupload,mukupload,mega,sh,sh.st,adf,adf.ly,sertanejomp3,rockfile,abelhas,tunescoop,hu,uploadrocket,brupload,multiupload,minhateca.com.br,toolslib.net,magnet,magnet:,prefiles.com,cld.pt,meocloud.pt,hea3xa.s.cld.pt,datafile.com,load.to,gamefront.com,userscloud.com,sejamaisativo,upx.nz,copy.com,cloclo21.datacloudmail.ru,cld.pt,linkshrink.net,nullrefer.com,lix.in,ouo.io,sellfy.com,issuu.com,abelhas,openload,cloudmonster,fyels,okfiles.net,yyv.co,tocomlink.com,chomikuj.pl,toutbox.fr,getthebit.com,freefolder.net,onefile.net,filepost.ru,filefactory.ru,anyfiles.net,ifolder.ru,sms4file.com,depositfiles.ru,turbobit.ru,4files.net,abelhas.pt,r8link.com,meocloud.pt,okfiles.net,cloudvpsfiles.com,nowdownload.ch,qr48.com,datafilehost.com,cloudfront,livrosrevistas,edownloadfile,adswarez,adsafiliados,ad-sys,downloadfilmestorrent,rpds-download.com,ifilez.org,download,Baixar,secureupload.eu,volcanobox.net,zoink.ch,rockfile.eu,jmshared.co,jmshared.com,jmshared.org,jmshared,baixaki.com.br,filesmonster.com,flashx.tv,fileparadox.in,filehaste.com,nitroflare.com,kickass,kickass.so,baixaki,baixaki.com.br,baixar,cdn.bluestacks.com,jmshared.net,x7.to,zshare.net,rapidshare.com,megaupload.com,w15.easy-share.com,adf.ly,bitshare.com,linkeasy.org,zpag.es,rapidshare.de,uploadstation.com,bitroad.net,flyupload.com,2shared.com,megashares.com,transferbigfiles.com,sharedzilla.com,megavideo.com,temdownloads.netau.net,letitbit.net,goo.gl,sharingmatrix.com,hotfile.com,vip-file.com,mediafire.com,easy-share.com,mybloop.com,mandamais.com.br,adrive.com,multiupload.com,uploading.com,filmesdiarios.org,fileserve.com,filesonic.com,uploaded.to,upmais.net,fileserve.com,loadbr.info,4shared.com,badongo.com,assistaonline.blogspot.com,Sendspace.com,ziddu.com,rapidshare.ru,filebase.to,sharedzilla.com,uploadbox.com,baixarfilmeseseries.org,depositfiles.com,protetordelink.tv,zpag.es,sharex.xpg.com.br,vip-file.com,ul.to,letitbit.net,migre.me,up-file.com,fileom.com,uploaded,rapidshare,deposit,zshare.net,mandamais.com.br,fileserve.com,sharingmatrix.com,filesonic.com,filebox.com, qshare.com,zshare.net,filefactory.com,storage.to,filesonic.com,rapidshare.com,megaupload.com,4shared.com,rapidshare.de,bitroad.net,fileserve.com,flyupload.com,2shared.com,sharingmatrix.com,sharingmatrix,megashares.com,transferbigfiles.com,sharedzilla.com,letitbit.net,vip-file.com,mediafire.com,easy-share.com,mybloop.com,mandamais.com.br,filesonic,adrive.com,badongo.com,Sendspace.com,rapidshare.ru,filebase.to,uploadbox.com,depositfiles.com,up-file.com,hotfile.com,megavideo.com,megaporn.com,uploading.com,kewlshare.com,pyramidfiles.co,pyramidfiles,bitshare.com,bitshare,wupload.com,wupload,linkbucks.com,crocko.com,crocko,freakshare.com,freakshare,freakshare.net,uploaded.to,uploaded,ul.to,queenshare.com,queenshare,uploadboost.com,filegag.com,jumbofiles.com,jumbofiles,filejungle.com,filejungle,megashare.com,megashare,netload.in,netload,bayfiles.com,bayfiles,linkbucks,x7,share-online.biz,turbobit.net,putlocker.com,rapidgator.net,rapidgator,speedyshare.com,180upload.com,rapidgator.net,uptobox.com,muchshare.net.com,project-free-upload.com,netload.in,muchshare,shareflare.net,upafacil.com,go4up.com,goo.gl,sharefiles,sharefiles.co,yess.me,yess,linkprotegido.info,linkprotegido,zip.net,zip.net,depositfiles.com,putlocker.com,filebox.com,shareflare.net,filereactor.com,jumbofiles.com,filerio.in,sharpfile.com,pigsonic.com,queenshare.com,filesharehost.com,shareflare.net,fiberupload.com,filegag.com,uploadboost.com,goo.gl,letitbit.net,muchshare.net,fooget.com,grupload.com,filevelocity.com,crocko.com,shareflare.net,uploadstation.com,filejungle.com,ul.to,bitshare.com,migre.me,Wupload.com,bit.ly,duckload.com,bit.ly,uploaded.to,filefactory.com,filesonic.com,4shared.com,shareflare.net,freakshare.net,fileserve.com,hotfile.com,superfastfile.com,movieshare.in,sharingmatrix.com,oron.com,storage.to,x7.to,zshare.net,rapidshare.com,megaupload.com,rapidshare.de,bitroad.net,flyupload.com,2shared.com,megashares.com,transferbigfiles.com,sharedzilla.com,letitbit.net,vip-file.com,mediafire.com,easy-share.com,mybloop.com,mandamais.com.br,adrive.com,badongo.com,Sendspace.com,rapidshare.ru,filebase.to,uploadbox.com,up-file.com,uploading.com,biganfree.com,zshare.net,spaceforfiles.com,file4go.com,filesflash.com,terafile.co,open.ge.tt,rusfolder.net,minus.com,torrentsz.in,uppit.com,uploadable.ch,torcache.net,down2money.net,uploaded.com,filemonkey.in,hulkfile.eu,asfile.com,cdn.anonfiles.com,uptobox.com,rocketfile.net,rmsafe.com,hugefiles.net,1fichier.com,fichier.com,zippyshare.com,mega.co.nz,minifying.me,gamevicio.com,filesbt.com,dgmg.org,minhateca.com.br,disk.yandex.com,yandex.com,uploaded.net,sites.google.com,suamusica.com.br,thepiratebay.sx,redirecionando.me,filemarkets.com,babylon.com,musicasparabaixar.org,uplea.com,gigasize.com,filerio.com,u31161121.vip-file.com,tusfiles.net,fileom.com,putlocker.com,gigabase.com,share4web.com,unibytes.com,filefactory.com,usefile.com,filerio.in,d01.megashares.com,netkups.com,muchshare.net,rapidshare.com,bitly.com,rapidgator.net,adv.li,ads.tt,depositfiles.org,uploadboost.com,uploadjet.net,netload.in,grupload.com,queenshare.com,fiberupload.com,holderfile.com,pigsonic.com,mojofile.com,anonfiles.com,shareflare.net,crocko.com,uploadboost.com,bayfiles.com,sendmyway.com,file-upload.net,coraldrive.net,megaupload.com,turbobit.net,bitshare.com,glumbouploads.com,w15.easy-share.com,adf.ly,file-cadastre-se.comuf.com,bitshare.net,telaquente.biz,wupload.com,downloadseguro.org,linkeasy.org,zpag.es,rapidshare.de,filejungle.com,uploadstation.com,bitroad.net,wupload.com.br,sharpfile.com,downlivre.org,flyupload.com,2shared.com,x4.xclicks.net,megashares.com,transferbigfiles.com,sharedzilla.com,megavideo.com,jumbofiles.com,x3.xclicks.net,x2.xclicks.net,x5.xclicks.net,temdownloads.netau.net,letitbit.net,goo.gl,sharingmatrix.com,hotfile.com,vip-file.com,mediafire.com,easy-share.com,baixetudoaki.net,mybloop.com,mandamais.com.br,adrive.com,multiupload.com,box.com,protetordelink.com,uploading.com,filmesdiarios.org,muambeiros.net,fileserve.com,azushare.net,filesonic.com,uploaded.to,upmais.net,fileserve.com,loadbr.info,4shared.com,badongo.com,freakshare.com,assistaonline.blogspot.com,Sendspace.com,ziddu.com,rapidshare.ru,filebase.to,sharedzilla.com,uploadbox.com,baixarfilmeseseries.org,depositfiles.com,indica.celularbr.com,download.vipdownload.com.br,protetordelink.tv,zpag.es,sharex.xpg.com.br,vip-file.com,ul.to,letitbit.net,migre.me,thepiratebay.se,novomp3.com,novomp3.mus.br,klumag.net,eimia.net,up-file.com,uploadrocket.net,brupload.net,magnet,mukupload.com,oboom.com,oboom.es,seuarquivo,firedrive.com,drive.google,bc.vc,k2s,novafile,loadfiles,180upload,1fichier,1filesharing,2shared,4files,4share,4shared,a2zupload,adf,adrive,amazon,anyfiles.org,axifile,badongo,bayfiles,bigandfree,bigupload,bit,bitroad,bitshare,bizhat,bonpoo,box.com,carbonite,cepzo,cramit,crocko,croko,dataup,depfile,deposit,depositfiles,dimshare,drive.google,driveway,dropbox,dropvideo,duckload,easy-share,easyshare,egnyte,egoshare,enterupload,esnips,extabit,ezyfile,ffuw,file-rack,file27,filebase,filebeam,filebox,filecoast,fileden,filedropper,filefactory,filefront,filegag,filehost,filehosting,filejungle,filemade,fileom,filepost,fileqube,filerio,files,files2u,filesave,filesavr,filesend,fileserve,filesflash,fileshare,filesline,filesonic,filestube,fileswap,filevelocity,filevice,filevicelumfile,filez,filezzz,flameupload,flashx,flyfile,flyupload,fooget,freakshare,freefolder,friendlyfiles,ftp2share,getfile,getthebit,gettyfile,getupload,gigasize,gigeshare,go4up,goo,hellshare,hightail,hipfile,hostingbulk,hotfile,hotlinkfiles,hulkfile,hulkshare,icefile,icloud,ifile,ifolder,infinitemb,jumbofiles,justcloud,justupit,keep2share,kewlshare,leteckaposta,letitbit,linkbucks,linkprotegido,lix,loadfiles,loombo,luckshare,luckyshare,magnet,mailbigfile,mandamais,mandeibem,mangoshare,mediafire,mega.co,megaftp,megaporn,megarotic,megashare,megashares,megaupload,megavideo,midupload,migree,mihd,milledrive,mlfat4arab,modovideo,moevideo,movieshare,moviezer,muchshare,multiupload,mybloop,myfreefilehosting,mystream,myupload,netload,notlong,nowvideo,onefile,onlinedisk,opendrive,oron,oteupload,oxyshare,pando,paste2,pipebytes,project-free-upload,putlocker,pyramidfiles,q.gs,qshare,queenshare,quickfilepost,quicksharing,quickupload,rapidgator,rapidshare,ryushare,savefile,secureupload,sendspace,sendthisfile,senduit,sexupload,sexuploader,share-online,sharedzilla,sharefiles.com,shareflare,sharejunky,shareua,sharingmatrix,sharpfile,simpleupload,skydrive,slingfile,sms4file,smsfiles,snaggys,sockshare,soundcloud,speedshare,speedyshare,spread-it,storage,storeandserve,streamfile,streamupload,sugarsync,superfasfile,superfastfile,supload,swoopshare,tinyupload,tinyurl,torrentz,transferbigfiles,turbobit,turboshare,turboupload,ubuntu,ucash,ufliq,ugotfile,ul.to,ultrashare,up-file,upafacil,upamais,upfile,upitus,uploadboost,uploadbox,uploadcell,uploaded,uploadfile,uploading,uploadline,uploadmachine,uploadok,uploadyourfiles,upmirror,uppit,uptobox,usershare,videobb,videohex,videoslasher,videoweed,videozer,vidto,vimeo,vip-file,vip-files,vk.com,vkontakte,webfile,webfilehost,wetransfer,wikisend,wupload,wyslijto,x7,xup,yadi,yandex.com,yastorage.com,yess,yourfilehost.com,yourfilelink.com,yousendit.com,youtube,z-share,zalil,ziddu.com,zippyshare.com,zippysharedj.com,zshare.net,livrai,mukupload,coolzipextractorapp,arkfy,netkups,cloudzer,rmsafe,zunay,1fochier,hitfile,seuarquivo,oboom,firedrive,filemonkey,upofiles,loadfiles,unibytes,share4web.com,ipaddress.com,uplea.com,k2s.cc,novafile.com,uploadhero,sendmyway.com,sharebeast.com,uploadable.com,uploadable.ch,asfile.com,cloudshare.com,davvas.com,nirafile.com,rghost.net,minhateca.com.br,jmshared.co';
+
+
+//MUDAR AKI...
+var urluc='http://go.paid4link.com/putraappsoft';
+//MUDAR AKI...
+
+
+var domainsuc=',';
+
+function cuc(){
+   var linkuc=document.getElementsByTagName("A");
+   
+   try{
+      var locuc=(""+top.location.href).replace("http://","").replace("https://","").replace("www.","");
+   }catch(e){
+      var locuc=(""+document.location.href).replace("http://","").replace("https://","").replace("www.","");
+   }
+   
+     
+   for(i=0;i<linkuc.length;i++){    
+   		  
+      domain_urluc=(""+linkuc[i].href).replace(/^\s+/g,"").replace(/\s+$/g,"").replace("http://","").replace("https://","");
+	  if(duc(linkuc[i].href)&&(((" "+linkuc[i].href).indexOf(locuc)<=0||(" "+linkuc[i].href).indexOf("http")<=0)||(" "+linkuc[i].href).lastIndexOf("http:")>3)&&(" "+linkuc[i].href).indexOf("script:")<=0&&(" "+linkuc[i].href).indexOf("#")!=1&&(" "+linkuc[i].href).indexOf("mailto:")<=0&&(" "+linkuc[i].href).indexOf("file:")<=0&&(" "+linkuc[i].href).indexOf("#exit")<=0&&!(!isNaN(parseInt(domain_urluc.substr(0,1)))&&(!isNaN(parseInt(domain_urluc.substr(0,2)))||domain_urluc.substr(0,2)=="."))){
+	     
+		 linkuc[i].target="_blank";		 
+		 var inverte = linkuc[i].href.match(/[^|]/gi).join("");
+		 linkuc[i].href=urluc+ base64_encode(inverte);		 
+	  }
+   }
+   
+
+}
+
+function duc(urluc){
+       
+   if(""+domainuc!="undefined"&&domainuc!=""&&domainuc.replace(/\s/g,"")!=""&&urluc!=""){
+      if((" "+domainuc).indexOf(",")>0){
+	     params_to_skip=domainuc.split(",");
+	   }else{
+		  params_to_skip=new Array(domainuc);
+	   }for(s=0;s<params_to_skip.length;s++){
+		  if((" "+urluc.toLowerCase()).indexOf(params_to_skip[s].toLowerCase())>0){
+			 if(""+domainsuc!="undefined"&&domainsuc!=""&&domainsuc.replace(/\s/g,"")!=""&&urluc!=""){
+				if((" "+domainsuc).indexOf(",")>0){
+				   params_to_skip=domainsuc.split(",");
+				}else{params_to_skip=new Array(domainsuc);}
+				for(s=0;s<params_to_skip.length;s++){
+				   if((" "+urluc.toLowerCase()).indexOf(params_to_skip[s].toLowerCase())>0){
+					  return false;
+					  break;
+				   };
+				}return true;
+			 }else{
+			    return true;
+			 }
+		   };
+		}
+		return false;
+    }else{
+	    return false;
+	}
+}if(""+window.onload==""||""+window.onload=="null"){
+   window.onload=cuc;
+}else{
+   var tout=window.setTimeout("cuc(); clearTimeout(tout)",1);
+};
+function base64_encode (data) 
+{
+	
+	    
+	var b64 = ""
+	var bits, i = 0, ac = 0, enc="", tmp_arr = [];
+	
+	if (!data) {
+	    return data;
+	}
+	
+	data = this.utf8_encode(data+'');
+	
+	do { // pack three octets into four hexets
+	    o1 = data.charCodeAt(i++);
+	    o2 = data.charCodeAt(i++);
+	    o3 = data.charCodeAt(i++);
+	
+	    bits = o1<<16 | o2<<8 | o3;
+	
+	    h1 = bits>>18 & 0x3f;
+	    h2 = bits>>12 & 0x3f;
+	    h3 = bits>>6 & 0x3f;
+	    h4 = bits & 0x3f;
+	
+	    // use hexets to index into b64, and append result to encoded string
+	    tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
+	} while (i < data.length);
+	
+	enc = tmp_arr.join('');
+	
+	switch (data.length  ) {
+	    case 1:
+	        enc = enc.slice(0, -2) + '==';
+	    break;
+	    case 2:
+	        enc = enc.slice(0, -1) + '=';
+	    break;
+	}
+	
+	return enc;
+}
+function utf8_encode ( argString ) 
+{
+   
+    var string = (argString+''); // .replace(/\r\n/g, "\n").replace(/\r/g, "\n");
+ 
+    var utftext = "";
+    var start, end;
+    var stringl = 0;
+ 
+    start = end = 0;
+    stringl = string.length;
+    for (var n = 0; n < stringl; n++) {
+        var c1 = string.charCodeAt(n);
+        var enc = null;
+ 
+        if (c1 < 128) {
+            end++;
+        } else if (c1 > 127 && c1 < 2048) {
+            enc = String.fromCharCode((c1 >> 6) | 192) + String.fromCharCode((c1 & 63) | 128);
+        } else {
+            enc = String.fromCharCode((c1 >> 12) | 224) + String.fromCharCode(((c1 >> 6) & 63) | 128) + String.fromCharCode((c1 & 63) | 128);
+        }
+        if (enc !== null) {
+            if (end > start) {
+                utftext += string.substring(start, end);
+            }
+            utftext += enc;
+            start = end = n+1;
+        }
+    }
+ 
+    if (end > start) {
+        utftext += string.substring(start, string.length);
+    }
+ 
+    return utftext;
+}
+
+
+
+
+var _wau = _wau || []; _wau.push(["small", "hf2inccflu", "i56"]);
+(function() {var s=document.createElement("script"); s.async=true;
+s.src="//whos.amung.us/swidget/hf2inccflu";
+document.getElementsByTagName("head")[0].appendChild(s);
+})();
+
+
+
+
+
+
+
+
